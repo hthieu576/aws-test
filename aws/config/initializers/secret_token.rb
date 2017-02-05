cd9bc1a8544f87df095a498341096959bc52933e
Hoang Trung Hieu <hoangtrunghieu576@gmail.com>
2017-02-05 23:33:06 +0700

    add new

---
+++ aws/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aws::Application.config.secret_token = '5491b87012e301cebca191a8479f470a95bc7f22785b772a42369cc10345d6be0249ccb130e279f085c69c09dad182726937ac171dec888a3a9269648b02a835'
